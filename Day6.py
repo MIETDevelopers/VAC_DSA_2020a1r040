10d2d2f5241a7129191f30382b356090463c410d
Aadhaar Koul <75715900+Aadhaar-debug@users.noreply.github.com>
2023-02-25 20:17:43 +0530

    Started doing the linked lists

---
+++ Day6.py
@@ -0,0 +1,41 @@
+
+# DAY 6
+
+# Mergesort
+'''
+basically in this way we use the concept of divide and conqur
+
+'''
+
+# def mergesort():
+#     if len(list) > 1:
+#         mid = len(list)//2
+
+#         # now we declare the left side of themergesorted
+#         # we will sore the left side values to the left list by using the concept of slicing
+
+#         L = list[:mid] # all teh values to the left of the mid in the list a re stored int he value
+#         R = list[mid:] # all the values to the right of the list are stoed in the i the R value
+
+#         # divide and conquer is only possible through recursion
+
+#         mergesort(L)
+#         mergesort(R)
+
+#         i = j = k = 0
+
+#         # Copy the left values and save them tot eh temp variable 
+
+#         while i < len(L) and j < len(R):
+#             if L[i] <= R[j]:
+#                 L[k]=L[i]
+#                 i+=1
+#             else:
+#                 L[k] = L[j]
+#                 j+=1
+
+#         while i < len(L):
+#             list[k] = L[i]
+
+
+#  LINKDE list
