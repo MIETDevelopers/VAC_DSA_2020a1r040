bc0861199b4bd72417cb42ddff0b5143b11761fd
Aadhaar Koul <75715900+Aadhaar-debug@users.noreply.github.com>
2023-02-25 20:13:10 +0530

    More Basic Stuff

---
+++ Day2.py
@@ -0,0 +1,171 @@
+# DAY 2
+
+
+# strings in pytohn are a sequence of characters
+# it is an immutable data type
+# means that that once you have created a string it cannot be changed
+# strings are mainly used to store and manipulate the text data.
+
+# the declaratio of string is given as below
+# string_1 = "this is just a test string"
+# string_2 = "this is another string statement."
+# print(string_1+string_2)
+
+
+# value = 101
+# print(type(value))
+
+
+# print(list(value))
+
+# this is an example of multiline comments
+
+# new  = '''ohoi matey his is your nightmare'''
+# print(new)
+
+
+# value = 'Ram'
+# for i in range(len(value)):z
+#     print("index"i,value[i])
+
+# print(value[2])
+
+'''
+
+Question for pratice
+email id by validating the following rules:
+
+'''
+
+# def verify(email):
+#     valid=False
+#     if email.split('@')[1] in ['gmail.com','yahoo.com']:
+#         valid=True
+#     count=0
+
+#     for c in range (len(email)-1):
+#         if email[c]==email[c+1]:
+#             count+=1
+#         if email[c]=='.':
+#             email.replace('.','+')
+#             valid = True
+#     if valid and count<2:
+#         print(email)
+#         return True
+
+# email = "raaju.ram@gmailcom"
+# print(verify(email))
+
+# lists are just dynamically sized arrays
+# it is a clooection of object encloded in suare brackets separeated by a coma
+# lis tis sequntia data which is used to store the collecrio of data
+# the syntax to declare a list is as follws
+
+# list_l = ['efef0','saefg']
+
+
+
+# list_l = [1,2,3,4,4]
+
+# def rotate(start,stop,list):
+#     while(start<stop):
+#         list(start),list(stop) = list(stop),list(start)
+#         start+=1
+#         stop-=1
+
+# list1 = [1,2,3,4,4]
+# key=2
+# rotate(0,key-1,list1)
+# rotate(key,len(list1)-1,list1)
+# rotate(0,len(list1)-1,list1)
+# print(list1)
+
+
+# def stepcleancount(totaltiles , pos):
+#     step count
+
+# tuples is a sequence of values stored in enclosed parenthesis
+# tuplea are created by using the sequence of values separated by comma with parenthesis
+# creating a tuple ithou the parenthesis is known as the tuple packet.
+
+# example of an empty tuple = new={}
+# value = {}
+# print(value)
+# value = ('Raju' , 'ram')
+# print(value)
+# value = tuple('raju')
+# print(value)
+
+# accessing tuple through indexes
+# we can also make this using the type casting method
+
+
+'''Question 
+you are given a text data where you would ahe to filter the daata on certain rules
+1) Get the first name 
+2)get the last four characters in null 'number
+3)get any text data from address with starts with number ''
+
+'''
+
+# name = 'Aadhaar koul'
+# for i in name :
+#     if()
+
+# consider that you have a list with values in nested from 
+# where if the values are present in the second list make it zero and find the sum'''
+
+
+
+# Dictionary is a collection if key , alue pairs used to store dat avalues like a map.
+# unlike other data types which hold a single value the key value os provided in a dictionary to make it more optimized .
+# Dictonary internally uses a adata str know as a hash map;
+# table which stores the values in keys.
+
+# hash map uses the hash functio in order to use the hash function
+# 3:300
+# hash function is something like equal to hash of 3 the internally the hash function is considered as the three or one 
+# value = 
+# {
+    
+# }
+
+# i =12
+# new = 1
+# while(new <= 20):
+#     print("*"*i)
+#     # while(new <= 20):
+#     #     print("*"*i)
+
+
+# dict = {
+#     string = "ioifnnbifnbiefnbe"
+#     for i in mydict.update(list)
+# }
+
+'''
+
+the major advantage of using a set is it is highl optimized method checking if an element is present or not 
+the elements are /....the big oh of N
+4,4,6,7,9
+big oh of 1 is the time complextity for calc the time comp of a program
+
+
+'''
+
+'''
+a set is creatd using a build in set method with an iterable object or a sequence by placcig the sequence inside the curly brace separated by comma'''
+
+# a set cannot have mutable elements like a list a dictionary as it is mutable .
+# 
+# set 1 = set()
+# empty set 
+# set1 = set("Hello world")
+
+# # it will not be stored in the same order 
+
+# print(set1)
+
+# print(set1)
+# sat1 = 
+
